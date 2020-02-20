e850a296ec2cfa1b99b0b04f5bc1e48748a7a7c7
anlkuma <35487655+anlkuma@users.noreply.github.com>
2020-02-20 18:16:01 +0530

    Add files via upload

---
+++ HIbernateInheritence/src/Employee.java
@@ -0,0 +1,23 @@
+package canno;  
+  
+  
+public class Employee {  
+private int id;  
+private String name;  
+  
+
+}  
+class Regular_Employee extends Employee{  
+private float salary;  
+private int bonus;  
+  
+
+}  
+
+
+class Contract_Employee extends Employee{  
+    private float pay_per_hour;  
+    private String contract_duration;  
+  
+
+}  
---
+++ HelloWorld/src/com/Hello1.java
@@ -0,0 +1,51 @@
+package com;
+
+import java.io.IOException;
+import javax.servlet.ServletConfig;
+import javax.servlet.ServletException;
+import javax.servlet.annotation.WebServlet;
+import javax.servlet.http.HttpServlet;
+import javax.servlet.http.HttpServletRequest;
+import javax.servlet.http.HttpServletResponse;
+
+/**
+ * Servlet implementation class Hello1
+ */
+@WebServlet("/Hello1")
+public class Hello1 extends HttpServlet {
+	private static final long serialVersionUID = 1L;
+
+    /**
+     * Default constructor. 
+     */
+    public Hello1() {
+        // TODO Auto-generated constructor stub
+    	
+    	System.out.println("HelloWorld");
+    }
+
+	/**
+	 * @see Servlet#init(ServletConfig)
+	 */
+	public void init(ServletConfig config) throws ServletException {
+		System.out.println("init");
+		// TODO Auto-generated method stub
+	}
+
+	/**
+	 * @see Servlet#destroy()
+	 */
+	public void destroy() {
+		// TODO Auto-generated method stub
+		System.out.println("destroy");
+	}
+
+	/**
+	 * @see HttpServlet#service(HttpServletRequest request, HttpServletResponse response)
+	 */
+	protected void service(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
+		// TODO Auto-generated method stub
+		System.out.println("service");
+	}
+
+}
