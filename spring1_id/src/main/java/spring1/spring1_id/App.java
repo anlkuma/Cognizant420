1d9bc1213db065af910c381720952d7d47b64a89
anlkuma <35487655+anlkuma@users.noreply.github.com>
2020-02-20 18:07:45 +0530

    Add files via upload

---
+++ spring1_id/src/main/java/spring1/spring1_id/App.java
@@ -0,0 +1,13 @@
+package spring1.spring1_id;
+
+/**
+ * Hello world!
+ *
+ */
+public class App 
+{
+    public static void main( String[] args )
+    {
+        System.out.println( "Hello World!" );
+    }
+}
