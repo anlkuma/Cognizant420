1320bcc4fa86a4e6ee6d6c051a1486eeb95cfe89
anlkuma <35487655+anlkuma@users.noreply.github.com>
2020-03-05 18:10:14 +0530

    Add files via upload

---
+++ Ipos/src/main/java/com/cts/project/Ipos/dao/IposRepository.java
@@ -0,0 +1,11 @@
+package com.cts.project.Ipos.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cts.project.Ipos.pojos.Ipos;
+
+public interface IposRepository extends CrudRepository<Ipos, Integer> {
+
+	
+
+}
