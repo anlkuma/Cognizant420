08d05f515679fb36f3386248b8980320ff76f03c
anlkuma <35487655+anlkuma@users.noreply.github.com>
2020-03-05 18:11:12 +0530

    Add files via upload

---
+++ ManageExchange/src/main/java/com/cts/project/ManageExchange/dao/ManageExchangeRepository.java
@@ -0,0 +1,11 @@
+package com.cts.project.ManageExchange.dao;
+
+import org.springframework.data.repository.CrudRepository;
+
+import com.cts.project.ManageExchange.pojos.ManageExchange;
+
+public interface ManageExchangeRepository extends CrudRepository<ManageExchange, Integer> {
+
+	
+
+}
